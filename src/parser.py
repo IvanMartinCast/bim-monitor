b00daa1df25ccfcde30c8016f0487836ed8cda7e
IvanMartinCast <154349060+IvanMartinCast@users.noreply.github.com>
2026-04-06 20:47:02 +0200

    Add document parsing function in parser.py

---
+++ src/parser.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def parse_document(item, country, region):
+    return {
+        "nombre": item["title"],
+        "pais": country,
+        "region": region,
+        "fecha_publicacion": datetime.today().strftime("%Y/%m/%d"),
+        "entrada_vigor": "",
+        "version": "N/A",
+        "estado": "vigente",
+        "fuente": item["url"]
+    }
