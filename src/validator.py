3c4f82fe0c198ba4bb5d55eb33cc79adcea3d5dd
IvanMartinCast <154349060+IvanMartinCast@users.noreply.github.com>
2026-04-09 00:26:27 +0200

    Enhance URL and title validation functions

--- src/validator.py
+++ src/validator.py
@@ -1,5 +1,16 @@
 def is_official(url):
-    return True
+    official_domains = [
+        ".gov", ".gob", ".gov.uk", ".eu",
+        "buildingsmart", "iso.org"
+    ]
+    return any(domain in url.lower() for domain in official_domains)
+
 
 def is_bim_relevant(title):
-    return True
+    keywords = [
+        "bim",
+        "iso 19650",
+        "building information",
+        "digital construction"
+    ]
+    return any(k in title.lower() for k in keywords)
