d936d71eece9d7260065842ea9cd55f7866e8be8
IvanMartinCast <154349060+IvanMartinCast@users.noreply.github.com>
2026-04-09 00:38:19 +0200

    Update validator.py

--- src/validator.py
+++ src/validator.py
@@ -1,16 +1,19 @@
 def is_official(url):
     official_domains = [
         ".gov", ".gob", ".gov.uk", ".eu",
-        "buildingsmart", "iso.org"
+        "buildingsmart", "iso.org",
+        "gov", "minister", "public"
     ]
-    return any(domain in url.lower() for domain in official_domains)
+    return any(d in url.lower() for d in official_domains)
 
 
 def is_bim_relevant(title):
     keywords = [
         "bim",
         "iso 19650",
-        "building information",
-        "digital construction"
+        "building information modeling",
+        "construction digital",
+        "standard",
+        "regulation"
     ]
     return any(k in title.lower() for k in keywords)
