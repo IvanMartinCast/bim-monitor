e7e76eeaee5e1408b8c4bc7cd28d588b295c40c7
IvanMartinCast <154349060+IvanMartinCast@users.noreply.github.com>
2026-04-08 23:17:52 +0200

    Update validator.py

--- src/validator.py
+++ src/validator.py
@@ -1,11 +1,5 @@
 def is_official(url):
-    whitelist = [
-        ".gov", ".gob", ".gov.uk", ".gc.ca",
-        "buildingsmart", "iso.org", "europa.eu"
-    ]
-    return any(w in url.lower() for w in whitelist)
-
+    return True
 
 def is_bim_relevant(title):
-    keywords = ["bim", "building information modeling", "iso 19650"]
-    return any(k in title.lower() for k in keywords)
+    return True
